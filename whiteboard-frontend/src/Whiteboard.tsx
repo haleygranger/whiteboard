ae25fcfe0f91932dc0078d3cdf26eccb7f22d2c7
haleygranger <hales@SCHOOLDESKTOP>
2025-03-19 13:32:09 -0400

    Add functional whiteboard

--- whiteboard-frontend/src/Whiteboard.tsx
+++ whiteboard-frontend/src/Whiteboard.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import TypingComponent from "./TypingComponent"; // Import TypingComponent
+import WhiteboardComponent from "./WhiteboardComponent";
 
 const Whiteboard: React.FC = () => {
   const [sessionId] = useState("session123");
   const [userName] = useState("user123");
 
   return (
-    <div>
-      <h1>Whiteboard</h1>
-      <TypingComponent sessionId={sessionId} userName={userName} />
+    <div>      
+      <WhiteboardComponent></WhiteboardComponent>
     </div>
   );
 };
