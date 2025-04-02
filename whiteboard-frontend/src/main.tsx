ca927e244f8d91a3d6769a261461f305ccc5a135
haleygranger <hales@SCHOOLDESKTOP>
2025-04-01 22:41:42 -0400

    I'll write more in a bit just want to test this stuff first

--- whiteboard-frontend/src/main.tsx
+++ whiteboard-frontend/src/main.tsx
@@ -1,26 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from 'react-oidc-context'; 
 import SignIn from "./SignIn";
 import Whiteboard from "./Whiteboard";
 import GuestSignIn from "./GuestSignIn";
 import GuestSignInWithSession from "./GuestSignInWithSession";
 import './App.css'
 
-const cognitoAuthConfig = {
-  authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_SV64Gr3VO", // Cognito domain
-  client_id: "1leh5p4ea4vq0imp5nksudclsa", 
-  redirect_uri: "https://main.dbiwmhwrvdu84.amplifyapp.com/whiteboard",  // redirect_uri
-  response_type: "code",  // OAuth?
-  scope: "email openid phone", 
-};
-
-
 const container = document.getElementById('root')!;
 ReactDOM.createRoot(container).render(
   <React.StrictMode>
-    <AuthProvider {...cognitoAuthConfig}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<SignIn />} />
@@ -29,6 +18,5 @@ ReactDOM.createRoot(container).render(
           <Route path="/GuestSignInSession" element={<GuestSignInWithSession />} />
         </Routes>
       </BrowserRouter>
-    </AuthProvider>
   </React.StrictMode>
 );
